1c302d69014a670ba8d5f5c365831cfcee417da7
A P CHANDAN <apchandu22@gmail.com>
2026-08-15 22:04:51 +0530

    Add TestNG failure listener for screenshots

---
+++ src/test/java/com/chandan/automation/utils/TestListener.java
@@ -0,0 +1,25 @@
+package com.chandan.automation.utils;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.chandan.automation.base.BaseTest;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Object instance = result.getInstance();
+
+        if (instance instanceof BaseTest) {
+            BaseTest baseTest = (BaseTest) instance;
+            String screenshotPath = ScreenshotUtil.capture(
+                    baseTest.driver,
+                    result.getMethod().getMethodName());
+
+            if (!screenshotPath.isEmpty()) {
+                System.out.println("Failure screenshot: " + screenshotPath);
+            }
+        }
+    }
+}

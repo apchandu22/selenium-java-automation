550b8e3e1171ac83f78f672398e7903eaadb836f
A P CHANDAN <apchandu22@gmail.com>
2026-08-30 21:08:04 +0530

    refactor: use BasePage and centralized test data in CartPage

--- src/test/java/com/chandan/automation/pages/CartPage.java
+++ src/test/java/com/chandan/automation/pages/CartPage.java
@@ -1,130 +1,40 @@
 package com.chandan.automation.pages;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CartPage {
+import com.chandan.automation.base.BasePage;
+import com.chandan.automation.data.TestData;
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class CartPage extends BasePage {
 
-    /*
-     * My Cart link in the header.
-     */
     private final By cartLink =
             By.xpath("//a[contains(normalize-space(.),'My Cart')]");
 
-    /*
-     * Checkout is a LINK on this site.
-     * Do NOT use input[type='submit'].
-     */
     private final By checkoutLink =
             By.xpath("//a[contains(normalize-space(.),'Check Out')]");
 
-    /*
-     * Product name.
-     */
     private final By greyJacketInCart =
-            By.xpath(
-                "//*[contains(normalize-space(.),'Grey jacket')]"
-            );
-
-    /*
-     * Cart count.
-     */
-    private final By cartWithItem =
-            By.xpath(
-                "//a[contains(normalize-space(.),'My Cart (1)')]"
-            );
+            By.xpath("//*[contains(normalize-space(.), '" + TestData.PRODUCT_NAME + "')]");
 
     public CartPage(WebDriver driver) {
-
-        this.driver = driver;
-
-        this.wait = new WebDriverWait(
-                driver,
-                Duration.ofSeconds(20)
-        );
+        super(driver);
     }
 
     public void openCart() {
-
-        WebElement cart = wait.until(
-                ExpectedConditions.presenceOfElementLocated(cartLink)
-        );
-
-        cart.click();
-
-        /*
-         * Wait for checkout to appear.
-         */
-        wait.until(
-                ExpectedConditions.presenceOfElementLocated(checkoutLink)
-        );
+        click(cartLink);
+        waitForVisibility(checkoutLink);
     }
 
     public boolean hasItems() {
-
-        /*
-         * First verify cart count.
-         */
-        try {
-
-            wait.until(
-                    ExpectedConditions.presenceOfElementLocated(
-                            cartWithItem
-                    )
-            );
-
-            return true;
-
-        } catch (Exception e) {
-
-            return false;
-        }
+        return isDisplayed(greyJacketInCart);
     }
 
     public boolean isCheckoutAvailable() {
-
-        /*
-         * Make sure the cart is open first.
-         */
-        try {
-
-            wait.until(
-                    ExpectedConditions.visibilityOfElementLocated(
-                            checkoutLink
-                    )
-            );
-
-            return true;
-
-        } catch (Exception e) {
-
-            return false;
-        }
+        return isDisplayed(checkoutLink);
     }
 
     public boolean isGreyJacketDisplayed() {
-
-        try {
-
-            wait.until(
-                    ExpectedConditions.visibilityOfElementLocated(
-                            greyJacketInCart
-                    )
-            );
-
-            return true;
-
-        } catch (Exception e) {
-
-            return false;
-        }
+        return isDisplayed(greyJacketInCart);
     }
 }

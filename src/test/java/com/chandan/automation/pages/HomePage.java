6dd39e387530deee89b85bd3328267d3113cd506
A P CHANDAN <apchandu22@gmail.com>
2026-08-30 21:07:46 +0530

    refactor: use BasePage in HomePage

--- src/test/java/com/chandan/automation/pages/HomePage.java
+++ src/test/java/com/chandan/automation/pages/HomePage.java
@@ -1,36 +1,20 @@
 package com.chandan.automation.pages;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage {
+import com.chandan.automation.base.BasePage;
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class HomePage extends BasePage {
 
     private final By greyJacket =
             By.cssSelector("a[href*='/products/grey-jacket']");
 
     public HomePage(WebDriver driver) {
-
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        super(driver);
     }
 
     public void openGreyJacket() {
-
-        wait.until(
-                ExpectedConditions.presenceOfElementLocated(greyJacket)
-        );
-
-        wait.until(
-                ExpectedConditions.visibilityOfElementLocated(greyJacket)
-        );
-
-        driver.findElement(greyJacket).click();
+        click(greyJacket);
     }
 }
